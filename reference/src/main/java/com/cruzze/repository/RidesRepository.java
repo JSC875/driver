f78fe6c552946499fe1bb1a9f5075a5d7966b37c
Sony-mol <nagunurisony22@gmail.com>
2025-07-24 17:06:24 +0530

    swipe goofline/offline backend sucess  Remove obsolete Docker, Maven configuration files, and Java entity/service classes to streamline project structure.

--- reference/src/main/java/com/cruzze/repository/RidesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cruzze.repository;
-
-
-
-import com.cruzze.entity.Rides;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RidesRepository extends JpaRepository<Rides, Long> {
-}
--- reference/src/main/java/com/cruzze/repository/VehicleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cruzze.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.cruzze.entity.Vehicles;
-
-public interface VehicleRepository extends JpaRepository<Vehicles, Long> {
-    Vehicles findByDriverId(Long driverId);
-}
